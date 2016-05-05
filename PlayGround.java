3a0fb9517aceeb2cf2585bcf3a37ec32e759cc60
rahul akula <rahul0611@gmail.com>
2016-05-05 00:35:23 -0500

    Initial commit

--- PlayGround.java
+++ PlayGround.java
@@ -9,34 +9,59 @@
  * @author rahula
  */
 public class PlayGround {
-    
+
     public static void main(String[] args) {
-        
+
         java.util.Scanner scannerObject = null;
-        
-        try{
-        
-        System.out.println("****** Fibonacci Series ********");
-        
-        System.out.println("***** Please enter N number to which you need Fibonacci Series *******");
-        
-        /** creating scanner object to read from system input **/
-        //TODO close scanner object
-        scannerObject = new java.util.Scanner(java.lang.System.in);
-        
-        /** read the number from keyboard  it can range a high value so i took long data type**/
-        long number = scannerObject.nextInt();
-        
-        /** closing the scanner ***/
-        scannerObject.close();
-        
-        }finally{
-        
+
+        try {
+
+            System.out.println("****** Fibonacci Series ********");
+
+            System.out.println("***** Please enter N to generate fibonacci series *******");
+
+            /**
+             * creating scanner object to read from system input *
+             */
+            //TODO close scanner object
+            scannerObject = new java.util.Scanner(java.lang.System.in);
+
+            /**
+             * read the number from keyboard it can range a high value so i took
+             * long data type*
+             */
+            long number = scannerObject.nextInt();
+
+            /**
+             * closing the scanner **
+             */
             scannerObject.close();
-        
+
+            int a = 0, b = 1;
+
+            System.out.println(a);
+            
+            while (number >= 0) {
+
+                System.out.println(b);
+
+                b = b + a;
+                a = b - a;
+                
+                /** decrease number for loop condition ***/
+                number--;
+
+            }
+
+        } finally {
+
+            
+            
+            if(scannerObject != null){
+                
+                scannerObject.close();
+                scannerObject = null;
+            }
         }
-        
-        
     }
-    
 }
